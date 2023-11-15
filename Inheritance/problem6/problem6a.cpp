d97a539cbeddf790856b09c5fcdd1fd2ac76251a
abdulrayan-17 <145854175+abdulrayan-17@users.noreply.github.com>
2023-11-15 20:49:50 +0530

    Add files via upload

---
+++ Inheritance/problem6/problem6a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    public:
+    A(int x) {
+        cout<<"A"<<x;
+    }
+};
+
+class B: public A{
+    public:
+    B(int x) {
+        cout<<"B"<<x;
+    }
+};
+
+class C: public B{
+    public:
+    C(int x,int y,int z):A(y),B(z) {
+        cout<<"C"<<x;
+    }
+}
+
+int main() {
+    B obj(2,5,9);
+    return 0;
+}
